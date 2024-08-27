9557f4f7ca7748116f4aa4b22aea57d5b23c2684
João Vitor Domingos Da Silva <res0132008@aluno.firjansenaisesi.com.br>
2024-08-27 11:47:19 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 const offset = 0;
 const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}pokemon?limit=10&offset=0`;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+fetch(url)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error)
+    });
